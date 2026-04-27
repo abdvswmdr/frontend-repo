cb191cc03acb7b3b59b56c7e61d6b847d9705826
Abdulswamad Rama <abdvswmdr@gmail.com>
2026-04-27 05:21:00 +0300

    refactor: replace var with const for improved variable scoping in index.js

--- api/metrics/index.js
+++ api/metrics/index.js
@@ -1,9 +1,9 @@
 (function (){
   'use strict';
 
-  var express = require("express")
-    , client  = require('prom-client')
-    , app     = express()
+  const express = require("express")
+  const client  = require('prom-client')
+  const app     = express()
 
   const metric = {
     http: {
@@ -14,29 +14,28 @@
   }
 
   function s(start) {
-    var diff = process.hrtime(start);
+    const diff = process.hrtime(start);
     return (diff[0] * 1e9 + diff[1]) / 1000000000;
   }
 
   function observe(method, path, statusCode, start) {
-    var route = path.toLowerCase();
+    const route = path.toLowerCase();
     if (route !== '/metrics' && route !== '/metrics/') {
-        var duration = s(start);
-        var method = method.toLowerCase();
-        metric.http.requests.duration.labels('front-end', method, route, statusCode).observe(duration);
+        const duration = s(start);
+        const lowerMethod = method.toLowerCase();
+        metric.http.requests.duration.labels('front-end', lowerMethod, route, statusCode).observe(duration);
     }
-  };
+  }
 
   function middleware(request, response, done) {
-    var start = process.hrtime();
+    const start = process.hrtime();
 
     response.on('finish', function() {
       observe(request.method, request.path, response.statusCode, start);
     });
 
     return done();
-  };
-
+  }
 
   app.use(middleware);
   app.get("/metrics", function(req, res) {
