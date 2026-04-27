7a77f77de7cd561059c6b0db1331b7a2f68d8652
Abdulswamad Rama <abdvswmdr@gmail.com>
2026-04-27 05:09:52 +0300

    fix: handle unhandled promise rejections to prevent application crashes

--- server.js
+++ server.js
@@ -52,6 +52,11 @@ app.use(user);
 
 app.use(helpers.errorHandler);
 
+process.on('unhandledRejection', function(reason, promise) {
+  console.error('Unhandled promise rejection:', reason);
+  process.exit(1);
+});
+
 var server = app.listen(process.env.PORT || 8079, function () {
   var port = server.address().port;
   console.log("App now running in %s mode on port %d", app.get("env"), port);
